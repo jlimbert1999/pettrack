fc0a4f89dcf1806145ccb930ee3732b47ad5c9d1
josel <jose@gmail.com>
2024-12-02 08:41:01 -0400

    correcion para deply

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000, '192.168.1.13');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
